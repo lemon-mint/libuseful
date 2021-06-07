9ebcf50453796beb381cb6a29e0479024c25fd1d
lemon-mint <55233766+lemon-mint@users.noreply.github.com>
2021-06-07 15:13:42 +0900

    fastrand

---
+++ fastrand.go
@@ -0,0 +1,12 @@
+package libuseful
+
+import (
+	_ "runtime"
+	_ "unsafe"
+)
+
+//go:linkname FastRand32 runtime.fastrand
+func FastRand32() uint32
+
+//go:linkname FastRand32n runtime.fastrandn
+func FastRand32n(n uint32) uint32
