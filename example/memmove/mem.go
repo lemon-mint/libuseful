2eb4f36c2a6f17304efd1b503b8dcbe42d9e7bb0
lemon-mint <55233766+lemon-mint@users.noreply.github.com>
2021-06-07 15:22:57 +0900

    memmove example

---
+++ example/memmove/mem.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"unsafe"
+
+	"github.com/lemon-mint/libuseful"
+)
+
+func main() {
+	var a = []byte{
+		0, 1, 2, 3,
+	}
+	var b = []byte{
+		0, 0, 0, 0,
+	}
+
+	A := (*reflect.SliceHeader)(unsafe.Pointer(&a))
+	B := (*reflect.SliceHeader)(unsafe.Pointer(&b))
+
+	fmt.Println("A:", a)
+	fmt.Println("B:", b)
+	libuseful.MemMove(unsafe.Pointer(B.Data), unsafe.Pointer(A.Data), 4)
+	fmt.Println("A:", a)
+	fmt.Println("B:", b)
+}
