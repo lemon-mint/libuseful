f76fb3abb25f9077df6c2ccabf1158f7cd0a6ac7
lemon-mint <ice.lemons.mint@gmail.com>
2021-10-01 06:50:12 +0900

    GetP

--- proc.go
+++ proc.go
@@ -13,15 +13,23 @@ func ProcPin() int
 func ProcUnpin()
 func GetM() unsafe.Pointer
 
+var gType reflect.Type = MustGetType("*runtime.g").Elem()
+var offset_g_goid uintptr = MustGetOffset(gType, "goid")
 var mType reflect.Type = MustGetType("*runtime.m").Elem()
 var offset_m_curg uintptr = MustGetOffset(mType, "curg")
 var offset_m_id uintptr = MustGetOffset(mType, "id")
-var gType reflect.Type = MustGetType("*runtime.g").Elem()
-var offset_g_goid uintptr = MustGetOffset(gType, "goid")
+var offset_m_p uintptr = MustGetOffset(mType, "p")
+var pType reflect.Type = MustGetType("*runtime.p").Elem()
+var offset_p_id uintptr = MustGetOffset(pType, "id")
 
 func GetG() unsafe.Pointer {
 	m := GetM()
 	return *(*unsafe.Pointer)(unsafe.Add(m, offset_m_curg))
+}
+
+func GetP() unsafe.Pointer {
+	m := GetM()
+	return *(*unsafe.Pointer)(unsafe.Add(m, offset_m_p))
 }
 
 func GetMID() int64 {
@@ -35,7 +43,6 @@ func GetGoID() int64 {
 }
 
 func GetPID() int {
-	id := ProcPin()
-	ProcUnpin()
-	return id
+	p := GetP()
+	return int(*(*int32)(unsafe.Add(p, offset_p_id)))
 }
